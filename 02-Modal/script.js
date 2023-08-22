1987d8c5365926719b905bfa0b2025318e4ede9b
Akbar Amin <akbaraminpk@gmail.com>
2023-08-22 13:24:20 +0500

    final commit - completed

--- 02-Modal/script.js
+++ 02-Modal/script.js
@@ -1,1 +1,32 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+// CLOSE MODAL FUNCTION
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// OPEN MODAL FUNCTION
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+// Close Modal functionality
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// Close modal by pressing ESC key
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
