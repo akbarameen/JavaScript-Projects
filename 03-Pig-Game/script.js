cdd051a1139e921611670ddb3df6fceee5e18582
Akbar Amin <akbaraminpk@gmail.com>
2023-08-24 14:08:29 +0500

    final commit - completed

--- 03-Pig-Game/script.js
+++ 03-Pig-Game/script.js
@@ -77,7 +77,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2. check if the scores is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
